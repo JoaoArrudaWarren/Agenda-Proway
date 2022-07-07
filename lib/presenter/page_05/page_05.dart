b08eff2d36dde6a3242014698ab9f35ea1ac6b28
Victor Hugo Vansuit <Victor.Vansuit@proway.treina>
2022-07-07 16:03:45 -0300

    [fix] font updated

---
+++ lib/presenter/page_05/page_05.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Weather App',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
