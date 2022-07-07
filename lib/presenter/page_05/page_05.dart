cb4edb28f22b2cdf5e951b55c3cb6c0a0e94b92e
Victor Hugo Vansuit <Victor.Vansuit@proway.treina>
2022-07-07 17:02:15 -0300

    [update] added app bar and clinic name

--- lib/presenter/page_05/page_05.dart
+++ lib/presenter/page_05/page_05.dart
@@ -1,11 +1,66 @@
 import 'package:flutter/material.dart';
+import '../home/home_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
       title: 'Weather App',
-      home: HomePage(),
+      home: PageFive(),
       debugShowCheckedModeBanner: false,
     ),
   );
+}
+
+class PageFive extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(229, 229, 229, 229),
+          title: const Center(
+            child: Text(
+              'Minhas Reservas',
+              style: TextStyle(
+                color: Color.fromARGB(255, 95, 117, 177),
+                fontSize: 22,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Sarala',
+              ),
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 10,
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      'Consulta Dra. Cecilia Maria\n',
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 10,
+                          ),
+                          child: Text(
+                            'Clínica do fodase',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
 }
