8b139eabfd0b033df4452a6cbf7272de416fa47c
Douglas Fidelis <Douglas.Fidelis@proway.treina>
2022-07-07 16:55:50 -0300

    [feat] up page_04

---
+++ lib/presenter/page_04/page_04.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar(),
+      body: Column(
+        children: [
+          Column(
+            //alignment: Alignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.arrow_back),
+                      color: const Color.fromARGB(255, 112, 107, 107),
+                      iconSize: 35,
+                    ),
+                    const Text(
+                      'Fazer uma reserva',
+                      style: TextStyle(
+                          color: Color.fromRGBO(95, 117, 177, 1),
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      width: 50,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.black,
+                height: 1,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TableCalendar(
+                  firstDay: DateTime.utc(2010, 10, 16),
+                  lastDay: DateTime.utc(2030, 3, 14),
+                  focusedDay: DateTime.now(),
+                ),
+              ),
+              Container(
+                color: Color.fromARGB(255, 112, 107, 107),
+                height: 1,
+              ),
+              Row(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Horários',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 112, 107, 107),
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Wrap(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 100,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(
+                              color: const Color.fromARGB(255, 112, 107, 107),
+                              width: 1),
+                        ),
+                        child: const Text(
+                          '09:00',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 112, 107, 107),
+                              fontSize: 19,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 100,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(
+                              color: const Color.fromARGB(255, 112, 107, 107),
+                              width: 1),
+                        ),
+                        child: const Text(
+                          '09:00',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 112, 107, 107),
+                              fontSize: 19,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 100,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(
+                              color: const Color.fromARGB(255, 112, 107, 107),
+                              width: 1),
+                        ),
+                        child: const Text(
+                          '09:00',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 112, 107, 107),
+                              fontSize: 19,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 100,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(
+                              color: const Color.fromARGB(255, 112, 107, 107),
+                              width: 1),
+                        ),
+                        child: const Text(
+                          '09:00',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 112, 107, 107),
+                              fontSize: 19,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
