102455c9a09889058caa205fa76d7318050c3403
João Pedro De Arruda Rocha <Joo.Rocha@proway.treina>
2022-07-08 08:47:01 -0300

    [feat]Adding AppBarBottom

--- lib/presenter/page_04/page_04.dart
+++ lib/presenter/page_04/page_04.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:table_calendar/table_calendar.dart';
+//import 'package:table_calendar/table_calendar.dart';
 
 class Page2 extends StatelessWidget {
   const Page2({Key? key}) : super(key: key);
