5a7848159eac9ad186977655339f13d850f28383
Carlos Aurélio Casanova de la Torre Ugarte <Carlos.Ugarte@proway.treina>
2022-07-11 10:33:38 -0300

    [change] reservas

--- lib/presenter/main.dart
+++ lib/presenter/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'home/home_page.dart';
+import 'reservas/reservas.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       title: 'Agenda Warren',
-      home: HomePage(),
+      home: Reservas(),
       debugShowCheckedModeBanner: false,
     ),
   );
